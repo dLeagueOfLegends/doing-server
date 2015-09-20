e0a9ff041275515609e61c5a6028c7ec41d572ca
dminor112 <285970228@qq.com>
2015-09-20 20:46:19 +0800

    add redis config

--- src/main/java/com/heros/doing/controller/HelloController.java
+++ src/main/java/com/heros/doing/controller/HelloController.java
@@ -5,17 +5,27 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.dminor.baselib.jedis.impl.SRedis;
 
 @Controller
 public class HelloController extends BaseController{
 	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 	
+	@Autowired
+	SRedis redis;
+	
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public void hello(HttpServletRequest request, HttpServletResponse response){
 		logger.error("++++++++++++==");
+		redis.set("test11", "123");
+		logger.error("++++++++++++==11, {}", redis.get("test11"));
 		this.printNoCache(response, "hello.");
 	}
 }
