93d8228804ff5298799bb3201749fe1c9c269bc2
dminor112 <285970228@qq.com>
2015-10-19 22:22:20 +0800

    fix hinernate current session

--- src/main/java/com/heros/doing/controller/BaseController.java
+++ src/main/java/com/heros/doing/controller/BaseController.java
@@ -1,7 +1,10 @@
 package com.heros.doing.controller;
 
 import java.io.IOException;
+import java.io.OutputStream;
+
 import javax.servlet.http.HttpServletResponse;
+
 import com.alibaba.fastjson.JSONObject;
 
 public class BaseController {
@@ -13,9 +16,20 @@ public class BaseController {
         response.setHeader("Pragma", "No-cache");
         response.setHeader("Cache-Control", "no-cache");
         response.setDateHeader("Expires", 0);
+        OutputStream out = null;
         try {
-            response.getWriter().print(result);
-            response.flushBuffer();
+        	out = response.getOutputStream();
+            byte[] bytes = result.getBytes( "utf-8" );
+            response.setStatus( 200 );
+            response.setContentLength( bytes.length );
+            response.setCharacterEncoding( "utf-8" );
+            response.setContentType( "application/json; charset=UTF-8" );
+            // response.setDateHeader("Expires", 0);
+            // response.setHeader("Pragma", "no-cache");
+            // // HTTP 1.1
+            // response.setHeader("Cache-Control", "No-cache");
+            out.write( bytes );
+            out.flush();
         } catch (IOException e) {
             System.out.println("print result error" + e);
         }
