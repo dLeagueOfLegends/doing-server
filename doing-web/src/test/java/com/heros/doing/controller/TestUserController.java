4ef8d91f8ec3db911fa7d5288661cb9b1e2d7e45
dminor112 <285970228@qq.com>
2016-01-12 00:45:01 +0800

    test node

--- doing-web/src/test/java/com/heros/doing/controller/TestUserController.java
+++ doing-web/src/test/java/com/heros/doing/controller/TestUserController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.junit.Test;
 
 import com.dminor.baselib.utils.HttpUtil;
+import com.dminor.baselib.utils.JsonUtil;
 
 public class TestUserController {
 	
@@ -14,9 +15,41 @@ public class TestUserController {
 		Map<String, String> params = new HashMap<String, String>();
 		Map<String, String> fileMap = new HashMap<String, String>();
 		params.put("imgSelect", "0");
-		params.put("data", "x7MGtny5nFDefAgTaRcckwhL3HbDlPq/gNwKZ77FMT22OWQAvB0Z3CK66askkYrpUb0LUB3jUJptsc9IMaRFPLZdT5PqpAfGxtXQwKU2SkToiXLBuOXpDVs37s73EpiEmRDsuaCggEcRvQ3aKcFzOg==");
+		params.put("data", "{\"nickName\" : \"张三\",\"sex\" : 1,\"age\" : 21,\"occupation\" : \"学生\",\"deviceCode\" : \"abcdefg\"}");
 		fileMap.put("imgFile", "D:\\test.jpg");
-//		System.out.println(HttpUtil.formUpload("http://127.0.0.1:3500/do/gi", params, fileMap));
+		System.out.println(HttpUtil.formUpload("http://127.0.0.1:3001/do/gi", params, fileMap));
+	}
+	
+	@Test
+	public void TestGetToken(){
+		Map<String, String> params = new HashMap<String, String>();
+		Map<String, String> fileMap = new HashMap<String, String>();
+		params.put("userId", "userId");
+		params.put("token", "tokenId");
+		System.out.println(HttpUtil.Get("http://127.0.0.1:3001/do/ke", "data=" + JsonUtil.ObjectToString(params)));
+	}
+	
+	@Test
+	public void TestSaveUserInfo(){
+		Map<String, String> params = new HashMap<String, String>();
+		Map<String, String> fileMap = new HashMap<String, String>();
+		params.put("token", "token");
+		params.put("user", "{\"nickName\" : \"张si\",\"sex\" : 1,\"age\" : 21,\"occupation\" : \"学生\",\"userId\" : \"82603a49\"}");
+		System.out.println(HttpUtil.formUpload("http://127.0.0.1:3001/userinfo/save", params, fileMap));
 	}
 
+	@Test
+	public void TestPublishDoing(){
+		Map<String, String> params = new HashMap<String, String>();
+		Map<String, String> fileMap = new HashMap<String, String>();
+		fileMap.put("img1", "D:\\test.jpg");
+		fileMap.put("img2", "D:\\test1.jpg");
+		params.put("imgCount", "2");
+		params.put("userId", "82603a49");
+		params.put("token", "82603a49");
+		params.put("message", "第一条doing");
+		params.put("position", "北京市");
+		params.put("device", "iphone5s");
+		System.out.println(HttpUtil.formUpload("http://127.0.0.1:3001/doing/pub", params, fileMap));
+	}
 }
